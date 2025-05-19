0d6c7f617a4c6cb793ed780f422f5f5eb5464e0a
nineeko <gkxm1013@naver.com>
2025-05-19 11:24:16 +0900

    25.05.19 am 11:23

--- chap01-02-vue-project/src/App.vue
+++ chap01-02-vue-project/src/App.vue
@@ -5,7 +5,10 @@ const num2 = ref(0);
 const result = ref(0);
 
 const sendPlus = async () => {
-  const response = await fetch(`http://localhost:8080/plus?num1=${num1.value}&num2=${num2.value}`);
+  /* NodePort에 설정 된 30001 번 포트로 백엔드 요청을 수행 */
+  // const response = await fetch(`http://localhost:30001/plus?num1=${num1.value}&num2=${num2.value}`);
+  /* Ingress를 적용하여 내부적으로 라우팅 될 주소로 수정 */
+  const response = await fetch(`/boot/plus?num1=${num1.value}&num2=${num2.value}`);
   const data = await response.json();
   console.log(`data : `, data);
   result.value = data.sum;
